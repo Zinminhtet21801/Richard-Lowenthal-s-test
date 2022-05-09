072252f732d16dda16754dc1ff3848d91071f509
Zin Min Htet <zetminhtin@gmail.com>
2022-05-09 08:13:34 +0630

    added heroku port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 app.get("/:qty", (req, res) => {
   const input = req.params.qty;
   console.log(input);
-  res.send(input)
+  res.send(input);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
